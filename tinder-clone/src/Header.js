a8d5abd478260916e040e38f056884e66b7aa1b7
sasuhas <suhas.a@eveniontechnologies.com>
2020-10-04 15:54:14 +0530

    initial commit

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header_icon"/>
+            </IconButton>
+            <img src={require("./Images/tinder-logo.png")} className="header_logo" alt=""></img>
+            <IconButton>
+                <ForumIcon fontSize="large" className="header_icon"/>
+            </IconButton>
+            
+        </div>
+    )
+}
+
+export default Header
